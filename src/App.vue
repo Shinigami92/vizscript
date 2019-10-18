530ee168946d6b4bea4caf7d4296c7b8022d8c01
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2019-10-18 11:05:25 +0200

    Init Main-UI-Frame

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+    <div id="main-control"></div>
+    <div id="sidenav-left">
+      <div class="title">Explorer</div>
+      <div class="content"></div>
     </div>
-    <router-view />
+    <div id="main-view">
+      <div id="main-toolbar"></div>
+      <div id="main-container">
+        <router-view />
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+$main-control-width: 48px;
+$sidenav-left-width: 300px;
+$main-toolbar-height: 35px;
+
+body {
+  margin: 0;
+  font-size: 13px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+    'Helvetica Neue', sans-serif;
 }
 
-#nav {
-  padding: 30px;
+#app {
+  display: flex;
+  color: rgb(227, 227, 227);
+  background-color: rgb(37, 37, 38);
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+#main-control {
+  background-color: rgb(33, 37, 43);
+  width: $main-control-width;
+  height: 100vh;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+#sidenav-left {
+  width: $sidenav-left-width;
+  height: 100vh;
+
+  .title {
+    height: $main-toolbar-height;
+    background-color: rgb(47, 52, 63);
+    color: rgb(187, 187, 187);
+    text-transform: uppercase;
   }
+
+  .content {
+    background-color: rgb(47, 52, 63);
+    height: calc(100vh - #{$main-toolbar-height});
+  }
+}
+
+#main-view {
+  height: 100vh;
+  width: calc(100vw - #{$main-control-width} - #{$sidenav-left-width});
+  overflow-y: auto;
+}
+
+#main-toolbar {
+  height: $main-toolbar-height;
+  background-color: rgb(47, 52, 63);
+}
+
+#main-container {
+  height: calc(100vh - #{$main-toolbar-height});
+  overflow-y: auto;
 }
 </style>
