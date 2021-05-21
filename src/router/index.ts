b350422ac19b44c0a34626b0138e6d64cec20ed9
Shinigami92 <chrissi92@hotmail.de>
2021-05-21 23:31:08 +0200

    Try hash history

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import Ast from '@/views/Ast.vue';
 import Code from '@/views/Code.vue';
 import Viz from '@/views/Viz.vue';
-import { createRouter, createWebHistory, Router, RouteRecordRaw } from 'vue-router';
+import { createRouter, createWebHashHistory, Router, RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -26,7 +26,7 @@ const routes: RouteRecordRaw[] = [
 ];
 
 const router: Router = createRouter({
-  history: createWebHistory(import.meta.env.PROD ? '/vizscript/' : undefined),
+  history: createWebHashHistory(import.meta.env.PROD ? '/vizscript/' : undefined),
   routes
 });
 
