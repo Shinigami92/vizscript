83dfea24ac0bc66392ab3dd9ca28e8702129bf3d
Shinigami92 <chrissi92@hotmail.de>
2021-05-21 23:14:43 +0200

    Make connection full quater compatible

--- src/components/viz-components/connections/VizEventConnection.vue
+++ src/components/viz-components/connections/VizEventConnection.vue
@@ -1,14 +1,47 @@
 <template lang="pug">
 .viz-event-connection.shape(ref='connection', :style='{ left: `${left}px`, top: `${top}px` }')
-  svg(:width='width', :height='height + 4')
-    path(
-      :d='`M 0 2 C ${40} 2, ${width - 40} ${height + 2}, ${width} ${height + 2}`',
-      stroke='white',
-      stroke-width='4',
-      fill='transparent'
-    )
-  icon.absolute(:style='{ left: `${-8}px`, top: `${-8 - 2}px` }') mdi-circle
-  icon.absolute(:style='{ left: `${width + -8}px`, top: `${height + -8 - 2}px` }') mdi-circle
+  template(v-if='xFlip')
+    template(v-if='yFlip')
+      svg(:width='width', :height='height + 4')
+        path(
+          :d='`M 0 2 C ${40} 2, ${width - 40} ${height + 2}, ${width} ${height + 2}`',
+          stroke='white',
+          stroke-width='4',
+          fill='transparent'
+        )
+      icon.absolute(:style='{ left: `${-8}px`, top: `${-8 - 2}px` }') mdi-circle
+      icon.absolute(:style='{ left: `${width + -8}px`, top: `${height + -8 - 2}px` }') mdi-circle
+    template(v-else)
+      svg(:width='width', :height='height + 4')
+        path(
+          :d='`M 0 ${height + 2} C ${40}  ${height + 2}, ${width - 40} 2, ${width} 2`',
+          stroke='white',
+          stroke-width='4',
+          fill='transparent'
+        )
+      icon.absolute(:style='{ left: `${-8}px`, top: `${height + -8 - 2}px` }') mdi-circle
+      icon.absolute(:style='{ left: `${width + -8}px`, top: `${-8 - 2}px` }') mdi-circle
+  template(v-else)
+    template(v-if='yFlip')
+      svg(:width='width', :height='height + 4')
+        path(
+          :d='`M ${width} 2 C ${width - 40} 2, ${40} ${height + 2}, 0 ${height + 2}`',
+          stroke='white',
+          stroke-width='4',
+          fill='transparent'
+        )
+      icon.absolute(:style='{ left: `${width + -8}px`, top: `${-8 - 2}px` }') mdi-circle
+      icon.absolute(:style='{ left: `${-8}px`, top: `${height + -8 - 2}px` }') mdi-circle
+    template(v-else)
+      svg(:width='width', :height='height + 4')
+        path(
+          :d='`M ${width} ${height + 2} C ${width - 40}  ${height + 2}, ${40} 2, 0 2`',
+          stroke='white',
+          stroke-width='4',
+          fill='transparent'
+        )
+      icon.absolute(:style='{ left: `${width + -8}px`, top: `${height + -8 - 2}px` }') mdi-circle
+      icon.absolute(:style='{ left: `${-8}px`, top: `${-8 - 2}px` }') mdi-circle
 </template>
 
 <script lang="ts">
@@ -30,9 +63,15 @@ export default defineComponent({
     const connection: Ref<HTMLDivElement | undefined> = ref();
     const left: ComputedRef<number> = computed(() => Math.min(props.modelValue.start.x, props.modelValue.end.x));
     const top: ComputedRef<number> = computed(() => Math.min(props.modelValue.start.y, props.modelValue.end.y));
-    const width: ComputedRef<number> = computed(() => props.modelValue.end.x - left.value);
-    const height: ComputedRef<number> = computed(() => props.modelValue.end.y - top.value);
-    return { connection, left, top, width, height };
+    const xFlip: ComputedRef<boolean> = computed(() => props.modelValue.end.x - left.value > 0);
+    const yFlip: ComputedRef<boolean> = computed(() => props.modelValue.end.y - top.value > 0);
+    const width: ComputedRef<number> = computed(() =>
+      xFlip.value ? props.modelValue.end.x - left.value : props.modelValue.start.x - left.value
+    );
+    const height: ComputedRef<number> = computed(() =>
+      yFlip.value ? props.modelValue.end.y - top.value : props.modelValue.start.y - top.value
+    );
+    return { connection, left, top, width, height, xFlip, yFlip };
   }
 });
 </script>
