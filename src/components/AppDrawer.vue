ad620ed3a633caf3db7b2feebdb2303cfdaaf102
Shinigami92 <chrissi92@hotmail.de>
2021-05-23 12:51:40 +0200

    Add Repo link

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -1,13 +1,18 @@
 <template lang="pug">
 aside.grid(class='grid-cols-[56px,1fr]')
-  .bg-background-400
+  .bg-background-400.flex.items-end
+    a(href='https://github.com/Shinigami92/vizscript', target='_blank')
+      icon.mx-1(:size='48') github
   .bg-background-500
     span.text-primary-50 Explorer
     div
 </template>
 
 <script lang="ts">
+import Icon from '@/components/Icon.vue';
 import { defineComponent } from 'vue';
 
-export default defineComponent({});
+export default defineComponent({
+  components: { Icon }
+});
 </script>
