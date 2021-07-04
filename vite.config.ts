633c8634ccf17bba93bea141ea0213e9b83ac0e0
Shinigami92 <chrissi92@hotmail.de>
2021-07-04 13:07:57 +0200

    Configure base for preview mode

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import WindiCSS from 'vite-plugin-windicss';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === 'production' ? '/vizscript/' : undefined,
+    base: mode === 'production' || process.env.npm_lifecycle_event === 'preview' ? '/vizscript/' : undefined,
     plugins: [vue(), WindiCSS()],
     resolve: {
       alias: {
