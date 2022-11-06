09a0e598e7a968aaff563d7a7b989dabdf8f6680
Shinigami92 <chrissi92@hotmail.de>
2022-11-06 16:40:51 +0100

    Fix deploy link

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -15,6 +15,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Shinigami92/vizscript.git master:gh-pages
+git push -f git@github.com:Shinigami92/vizscript.git main:gh-pages
 
 cd -
